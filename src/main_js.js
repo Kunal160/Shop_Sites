a4e5abd6bd0e676330c1d34615a2824749f92412
Kunal160 <90954669+Kunal160@users.noreply.github.com>
2021-09-18 14:56:46 +0530

    Add files via upload

--- src/main_js.js
+++ src/main_js.js
@@ -1,24 +1,19 @@
-/*Database*/
-//constructor
-console.log(5+6);
-
 /*login data validate*/
 const loginData= document.getElementById("login-form");
+/*function handleForm(event) { event.preventDefault(); }
+loginData.addEventListener('submit', handleForm); */
+
 const loginButton = document.getElementById("login-button");
-
-/*eventlistner*/
-loginButton.addEventListener("click", checkLogin);
-
+loginButton.addEventListener('click', checkLogin);
 function checkLogin() {
     const idName = loginData.loginname.value;
     const idPassword = loginData.loginpassword.value;
-    
     if((idName === "alias160") && (idPassword === "alias160")) {
-        window.location = "123.html/";
-        console.log();
-        alert("hello");
-        return false;      
+        window.location = "index.html";
+        alert("hello");             
     } else {
         alert("Username/Password is incorrect");
     }
+   
+   // return true;
 }
